69131697d2a1ca8fa3dd504804f1eaa8b4829b6e
Emmanuel Delgado <delgaemma9912@gmail.com>
2026-06-24 12:19:55 -0600

    disminución de tamñao número en la gráfica actualización coordinado 2

--- cliente/avances-pazstor/src/components/5 Dígitos/León/Coordinado/Gráficas/GráficaCoordinado L-2/GraficaCoordinadoL2.jsx
+++ cliente/avances-pazstor/src/components/5 Dígitos/León/Coordinado/Gráficas/GráficaCoordinado L-2/GraficaCoordinadoL2.jsx
@@ -77,7 +77,7 @@ const GraficaCoordinadoL2 = () => {
     dataLabels: {
       enabled: true,
       style: {
-        fontSize: "60px", // <--- Ajusta este tamaño a tu gusto (ej. '18px', '20px')
+        fontSize: "30px", // <--- Ajusta este tamaño a tu gusto (ej. '18px', '20px')
         fontFamily: "Inter, sans-serif",
         fontWeight: "bold",
         colors: ["#fff"], // Mantiene el color blanco
