446929f1c2e53afbe61f961b5c0fd8ca42a06691
Emmanuel Delgado <delgaemma9912@gmail.com>
2026-08-19 15:58:11 -0600

    Se realiza corrección personal.routes 19/08/2026 3:58

--- servidor/src/routes/Personal/personal.routes.js
+++ servidor/src/routes/Personal/personal.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import {obtenerPersonalPorDepartamento} from '../../controllers/Personal/Personal.controller.js';
+import {obtenerPersonalPorDepartamento} from '../../controllers/Personal/personal.controller.js';
 
 const router = Router();
 router.get('/personaldepto', obtenerPersonalPorDepartamento);
